HolyTigerGod/Voidcrew

Cleans up + Improves bows, Sorts files, Adds the Divine Archer clothing, weapon, rite (#74811)
## About The Pull Request

### Divine Archer 🏹 


![image](https://user-images.githubusercontent.com/40974010/232647927-aace69ea-bda8-4ec9-9bf1-60140034fbb3.png)

Adds a new chaplain weapon and suit of armor, the divine archer. It's an
orderable set of armor, but provides less armor than the rest, but you
get more pieces of armor (boots, bracer, undersuit).

The divine bow comes with a quiver that holds holy arrows. The holy
arrows come with bane support, dealing critical damage to revenants.

### Bow Features ⭐ 

- arrows can now be dipped in poison

### Bow Improvements 🔧 

- bows now drop their arrow when you put them on your back while nocking
a bow
- bows give feedback for trying to draw without a nocked arrow
- codewise, bows support subtypes much better. They still have
hard-sprited loaded arrows, but one day that'll change.

## Why It's Good For The Game

Yeah, we could add null rod #2342 that does almost the same as the
others, or we could have a unique bow weapon!

Player Dev Project thread:
https://discord.com/channels/326822144233439242/1093521091957370940/1093521091957370940

## Changelog
:cl: tralezab code, Drag for the commission and player project, cre#0484
for their spritework
add: Divine Archer Armor and Weapon
qol: Bows give more feedback when you're doing something wrong, like
trying to draw without a nocked arrow
code: Sorted files, cleaned bow code up to allow subtypes
/:cl:

code/modules/projectiles/guns/ballistic/bows/_bow.dm
@@ -1,0 +1,79 @@
+
+/obj/item/gun/ballistic/bow
+	icon = 'icons/obj/weapons/guns/bows/bows.dmi'
+	lefthand_file = 'icons/mob/inhands/weapons/bows_lefthand.dmi'
+	righthand_file = 'icons/mob/inhands/weapons/bows_righthand.dmi'
+	icon_state = "bow"
+	inhand_icon_state = "bow"
+	base_icon_state = "bow"
+	load_sound = null
+	fire_sound = null
+	mag_type = /obj/item/ammo_box/magazine/internal/bow
+	force = 15
+	attack_verb_continuous = list("whipped", "cracked")
+	attack_verb_simple = list("whip", "crack")
+	weapon_weight = WEAPON_HEAVY
+	w_class = WEIGHT_CLASS_BULKY
+	internal_magazine = TRUE
+	cartridge_wording = "arrow"
+	bolt_type = BOLT_TYPE_NO_BOLT
+	/// whether the bow is drawn back
+	var/drawn = FALSE
+
+/obj/item/gun/ballistic/bow/update_icon_state()
+	. = ..()
+	icon_state = chambered ? "[base_icon_state]_[drawn ? "drawn" : "nocked"]" : "[base_icon_state]"
+
+/obj/item/gun/ballistic/bow/proc/drop_arrow()
+	drawn = FALSE
+	if(chambered)
+		chambered.forceMove(drop_location())
+		magazine.get_round(keep = FALSE)
+		chambered = null
+	update_appearance()
+
+/obj/item/gun/ballistic/bow/chamber_round(keep_bullet = FALSE, spin_cylinder, replace_new_round)
+	if(chambered || !magazine)
+		return
+	if(magazine.ammo_count())
+		chambered = magazine.get_round(TRUE)
+		chambered.forceMove(src)
+
+/obj/item/gun/ballistic/bow/attack_self(mob/user)
+	if(!chambered)
+		balloon_alert(user, "no arrow nocked!")
+	else
+		balloon_alert(user, "[drawn ? "string released" : "string drawn"]")
+		drawn = !drawn
+	update_appearance()
+
+/obj/item/gun/ballistic/bow/afterattack(atom/target, mob/living/user, flag, params, passthrough = FALSE)
+	. |= AFTERATTACK_PROCESSED_ITEM
+	if(!chambered)
+		return
+	if(!drawn)
+		to_chat(user, span_warning("Without drawing the bow, the arrow uselessly falls to the ground."))
+		drop_arrow()
+		update_appearance()
+		return
+	drawn = FALSE
+	. = ..() //fires, removing the arrow
+	update_appearance()
+
+/obj/item/gun/ballistic/bow/equipped(mob/user, slot, initial)
+	. = ..()
+	if(slot == ITEM_SLOT_BACK && chambered)
+		balloon_alert(user, "the arrow falls out!")
+		drop_arrow()
+		drawn = FALSE
+		update_appearance()
+
+/obj/item/gun/ballistic/bow/shoot_with_empty_chamber(mob/living/user)
+	return //no clicking sounds please
+
+/obj/item/ammo_box/magazine/internal/bow
+	name = "bowstring"
+	ammo_type = /obj/item/ammo_casing/caseless/arrow
+	max_ammo = 1
+	start_empty = TRUE
+	caliber = CALIBER_ARROW

Monkestation/Monkestation2.0

code/modules/projectiles/guns/ballistic/bows/_bow.dm
@@ -1,0 +1,79 @@
+
+/obj/item/gun/ballistic/bow
+	icon = 'icons/obj/weapons/guns/bows/bows.dmi'
+	lefthand_file = 'icons/mob/inhands/weapons/bows_lefthand.dmi'
+	righthand_file = 'icons/mob/inhands/weapons/bows_righthand.dmi'
+	icon_state = "bow"
+	inhand_icon_state = "bow"
+	base_icon_state = "bow"
+	load_sound = null
+	fire_sound = null
+	accepted_magazine_type = /obj/item/ammo_box/magazine/internal/bow
+	force = 15
+	attack_verb_continuous = list("whipped", "cracked")
+	attack_verb_simple = list("whip", "crack")
+	weapon_weight = WEAPON_HEAVY
+	w_class = WEIGHT_CLASS_BULKY
+	internal_magazine = TRUE
+	cartridge_wording = "arrow"
+	bolt_type = BOLT_TYPE_NO_BOLT
+	/// whether the bow is drawn back
+	var/drawn = FALSE
+
+/obj/item/gun/ballistic/bow/update_icon_state()
+	. = ..()
+	icon_state = chambered ? "[base_icon_state]_[drawn ? "drawn" : "nocked"]" : "[base_icon_state]"
+
+/obj/item/gun/ballistic/bow/proc/drop_arrow()
+	drawn = FALSE
+	if(chambered)
+		chambered.forceMove(drop_location())
+		magazine.get_round(keep = FALSE)
+		chambered = null
+	update_appearance()
+
+/obj/item/gun/ballistic/bow/chamber_round(keep_bullet = FALSE, spin_cylinder, replace_new_round)
+	if(chambered || !magazine)
+		return
+	if(magazine.ammo_count())
+		chambered = magazine.get_round(TRUE)
+		chambered.forceMove(src)
+
+/obj/item/gun/ballistic/bow/attack_self(mob/user)
+	if(!chambered)
+		balloon_alert(user, "no arrow nocked!")
+	else
+		balloon_alert(user, "[drawn ? "string released" : "string drawn"]")
+		drawn = !drawn
+	update_appearance()
+
+/obj/item/gun/ballistic/bow/afterattack(atom/target, mob/living/user, flag, params, passthrough = FALSE)
+	. |= AFTERATTACK_PROCESSED_ITEM
+	if(!chambered)
+		return
+	if(!drawn)
+		to_chat(user, span_warning("Without drawing the bow, the arrow uselessly falls to the ground."))
+		drop_arrow()
+		update_appearance()
+		return
+	drawn = FALSE
+	. = ..() //fires, removing the arrow
+	update_appearance()
+
+/obj/item/gun/ballistic/bow/equipped(mob/user, slot, initial)
+	. = ..()
+	if(slot == ITEM_SLOT_BACK && chambered)
+		balloon_alert(user, "the arrow falls out!")
+		drop_arrow()
+		drawn = FALSE
+		update_appearance()
+
+/obj/item/gun/ballistic/bow/shoot_with_empty_chamber(mob/living/user)
+	return //no clicking sounds please
+
+/obj/item/ammo_box/magazine/internal/bow
+	name = "bowstring"
+	ammo_type = /obj/item/ammo_casing/caseless/arrow
+	max_ammo = 1
+	start_empty = TRUE
+	caliber = CALIBER_ARROW