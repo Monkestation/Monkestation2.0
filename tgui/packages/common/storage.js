3cd22e96704a35f98c3519c90fbd7815402f090a
Lucy <lucy@absolucy.moe>
2024-09-07 22:17:42 -0400

    Foward more 516 changes from Paradise

--- tgui/packages/common/storage.js
+++ tgui/packages/common/storage.js
@@ -71,15 +71,15 @@ class HubStorageBackend {
     }
   }
 
-  async set(key, value) {
+  set(key, value) {
     window.hubStorage.setItem('monke-' + key, JSON.stringify(value));
   }
 
-  async remove(key) {
+  remove(key) {
     window.hubStorage.removeItem('monke-' + key);
   }
 
-  async clear() {
+  clear() {
     window.hubStorage.clear();
   }
 }
