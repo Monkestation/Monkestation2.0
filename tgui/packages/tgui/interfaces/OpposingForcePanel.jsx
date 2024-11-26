3002787139281fa7aab734f247b27ea8e410b78a
Kmla <117773464+Kmla2@users.noreply.github.com>
2024-11-26 16:57:29 +0100

    The home --> rules page too

--- tgui/packages/tgui/interfaces/OpposingForcePanel.jsx
+++ tgui/packages/tgui/interfaces/OpposingForcePanel.jsx
@@ -223,7 +223,7 @@ export const OpposingForceTab = (props) => {
               </a>
             </Stack.Item>
             <Stack.Item>
-              <a href="https://wiki.monkestation.com/en/home">
+              <a href="https://wiki.monkestation.com/info/rules">
                 <Button
                   icon="question"
                   color="yellow"
