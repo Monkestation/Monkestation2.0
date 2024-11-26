536b27e137ab98b9c2e3236c0196a6573c87bda3
Kmla <117773464+Kmla2@users.noreply.github.com>
2024-11-26 16:45:50 +0100

    Fix the opfor panel links

--- tgui/packages/tgui/interfaces/OpposingForcePanel.jsx
+++ tgui/packages/tgui/interfaces/OpposingForcePanel.jsx
@@ -203,7 +203,7 @@ export const OpposingForceTab = (props) => {
           </Stack>
           <Stack>
             <Stack.Item>
-              <a href="https://wiki.monkestation.com/en/opfor">
+              <a href="https://wiki.monkestation.com/en/info/opfor">
                 <Button
                   icon="info"
                   color="orange"
@@ -213,7 +213,7 @@ export const OpposingForceTab = (props) => {
               </a>
             </Stack.Item>
             <Stack.Item>
-              <a href="https://wiki.monkestation.com/en/opfor">
+              <a href="https://wiki.monkestation.com/en/info/opfor">
                 <Button
                   icon="wrench"
                   color="red"
