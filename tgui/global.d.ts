d2537ec13570d25d53f7435081b7264d33782d76
Lucy <lucy@absolucy.moe>
2024-12-20 21:36:16 -0500

    Port my implementation from https://github.com/tgstation/tgstation/pull/88624

--- tgui/global.d.ts
+++ tgui/global.d.ts
@@ -52,6 +52,11 @@ type ByondType = {
   TRIDENT: number | null;
 
   /**
+   * Version of Blink engine of WebView2. Null if N/A.
+   */
+  BLINK: number | null;
+
+  /**
    * True if browser is IE8 or lower.
    */
   IS_LTE_IE8: boolean;
@@ -210,4 +215,13 @@ interface Window {
   Byond: ByondType;
   __store__: Store<unknown, AnyAction>;
   __augmentStack__: (store: Store) => StackAugmentor;
+
+  // IE IndexedDB stuff.
+  msIndexedDB: IDBFactory;
+  msIDBTransaction: IDBTransaction;
+
+  // 516 byondstorage API.
+  hubStorage: Storage;
+  byondStorage: Storage;
+  serverStorage: Storage;
 }
