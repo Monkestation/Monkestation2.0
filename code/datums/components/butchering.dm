HolyTigerGod/Voidcrew

Removes holographic monkey species (#81606)
## About The Pull Request

Split this off from https://github.com/tgstation/tgstation/pull/81598 in
hopes to keep it as atomic as I can.

Genericizes the things that holographic monkey species existed to
replace, so now we don't have to worry about having to copy paste this
to any future mob later down the line.

## Why It's Good For The Game

Removes a monkey species subtype, and a pretty bad one at that.
Preferably makes hologram mobs more consistent with eachother and
prevents copy paste happening everywhere in the future if any new mobs
ever gets added to the holodeck.

## Changelog

:cl:
refactor: Holographic mobs now gives better feedback to players and
should more consistently not give any drops.
/:cl:

code/datums/components/butchering.dm
@@ -107,6 +107,12 @@
 	var/final_effectiveness = effectiveness - target.butcher_difficulty
 	var/bonus_chance = max(0, (final_effectiveness - 100) + bonus_modifier) //so 125 total effectiveness = 25% extra chance
 
+	if(target.flags_1 & HOLOGRAM_1)
+		butcher.visible_message(span_notice("[butcher] tries to butcher [target], but it vanishes."), \
+			span_notice("You try to butcher [target], but it vanishes."))
+		qdel(target)
+		return
+
 	for(var/result_typepath in target.butcher_results)
 		var/obj/remains = result_typepath
 		var/amount = target.butcher_results[remains]

Monkestation/Monkestation2.0

code/datums/components/butchering.dm
@@ -106,6 +106,13 @@
 	var/turf/T = meat.drop_location()
 	var/final_effectiveness = effectiveness - meat.butcher_difficulty
 	var/bonus_chance = max(0, (final_effectiveness - 100) + bonus_modifier) //so 125 total effectiveness = 25% extra chance
+
+	if(meat.flags_1 & HOLOGRAM_1)
+		butcher.visible_message(span_notice("[butcher] tries to butcher [meat], but it vanishes."), \
+			span_notice("You try to butcher [meat], but it vanishes."))
+		qdel(meat)
+		return
+
 	for(var/V in meat.butcher_results)
 		var/obj/bones = V
 		var/amount = meat.butcher_results[bones]