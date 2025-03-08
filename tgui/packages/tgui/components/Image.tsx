001c0cd7fc743a1c1cf4c7b34b5ec1dbf0519589
Lucy <lucy@absolucy.moe>
2025-03-07 23:00:00 -0500

    remove outdated comment

--- tgui/packages/tgui/components/Image.tsx
+++ tgui/packages/tgui/components/Image.tsx
@@ -50,7 +50,7 @@ export class Image extends Component<Props> {
       ...rest
     } = this.props;
 
-    /* Remove -ms-interpolation-mode with Byond 516. -webkit-optimize-contrast is better than pixelated */
+    /* Remove -ms-interpolation-mode with Byond 516. */
     const computedProps = computeBoxProps({
       style: {
         '-ms-interpolation-mode': fixBlur ? 'nearest-neighbor' : 'auto',
