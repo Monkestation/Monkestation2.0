HolyTigerGod/Voidcrew

Make too low vols and no soundin scream for playsound instead of failing silently (#89746)
…ing silently
## About The Pull Request
While reviewing the recent sound optimization PR I noticed that a bunch
of checks here really make no sense to just fail silently when nobody
should be doing this in the first place

Immediately started screaming on run so thats a good sign

## Changelog
:cl:
code: previously silent failures due to empty sounds or too low volumes
will now create a runtime in the runtime log
fix: some footstep sounds like robotic or slimes are no longer silent
/:cl:

---------

Co-authored-by: TiviPlus <572233640+TiviPlus@users.noreply.com>

code/modules/spells/spell.dm
@@ -378,7 +378,8 @@
 
 	///even INVOCATION_NONE should go through this because the signal might change that
 	invocation(invoker)
-	playsound(invoker, sound, 50, vary = TRUE)
+	if(sound)
+		playsound(invoker, sound, 50, vary = TRUE)
 
 /// The invocation that accompanies the spell, called from spell_feedback() before cast().
 /datum/action/cooldown/spell/proc/invocation(mob/living/invoker)

Monkestation/Monkestation2.0

code/modules/spells/spell.dm
@@ -350,7 +350,7 @@
 	///even INVOCATION_NONE should go through this because the signal might change that
 	invocation()
 	if(sound)
-		playsound(get_turf(owner), sound, 50, TRUE)
+		playsound(owner, sound, 50, vary = TRUE)
 
 /// The invocation that accompanies the spell, called from spell_feedback() before cast().
 /datum/action/cooldown/spell/proc/invocation()