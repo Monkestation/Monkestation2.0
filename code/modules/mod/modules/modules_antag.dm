HolyTigerGod/Voidcrew

New Syndicate Stealth MODule: Wraith. (#86449)
## About The Pull Request

Introduces a new MODule in the uplink, makes the user transperent and
grants the ability to siphon light sources to recharge your suit.

Ingame demonstration: https://www.youtube.com/watch?v=bhXNOAMDy4U

## Why It's Good For The Game

I've been playing a ton of Splinter Cell and Intravenous recently and
this random idea popped in my head.

"Wouldn't it be cool if traitors could blend in the darkness to get a
jump on their opponents?"

Also unrelated

"Wouldn't it be cool if tots had a tool to recharge their suit that
didn't involve sitting in a pod for 10 minutes?"

This PR introduces a new module to the uplink, the Wraith.

It comes with a passive and active component.

Passively it works exactly like the crew version of the cloaking module
with just a couple of differences.

1) Doesn't need to be manually activated, if you lose the cloak it's
regained after 5 seconds.

2) Lower stealth alpha value( how trasperent you are basically),
slightly less visible than the crew version, not as good the ninja
module however , I tuned it just enough so that you are more or less
undetectable in the dark.

The active component of the module lets you destroy stationary lights to
recharge your suit power, if used on handheld or borg lights it turns
them off for a minute.

**Why do we need this module when we already have the stealth implant
and the chameleon projector?**

I can think of a few reasons.

1) MODsuits were designed to be customizible, traitor suits range
between 6 to 16 TC, having to invest in a 7-8 TC item after you already
bought a suit is fairly expensive.

2) This MODule would be a better fit for ambushes, as it doesn't have
the *uncloaking* delay of its counterparts.

It is however considerably worse if you get caught, as the cloak is
disrupted on bump or damage.

3) It has better interactions with the sandbox.

Lights can go out for 

code/modules/mod/modules/modules_antag.dm
@@ -558,3 +558,63 @@
 
 /obj/item/gun/medbeam/mod
 	name = "MOD medbeam"
+
+/obj/item/mod/module/stealth/wraith
+	name = "MOD Wraith Cloaking Module"
+	desc = "A more destructive adaptation of the stealth module."
+	icon_state = "cloak_traitor"
+	stealth_alpha = 30
+	module_type = MODULE_ACTIVE
+	cooldown_time = 2 SECONDS
+
+/obj/item/mod/module/stealth/wraith/on_select_use(atom/target)
+	. = ..()
+	if(!. || target == mod.wearer)
+		return
+	if(get_dist(mod.wearer, target) > 6)
+		balloon_alert(mod.wearer, "can't reach that!")
+		return
+	if(istype(target, /obj/machinery/power/apc)) //Bit too strong for a module so this is blacklisted
+		balloon_alert(mod.wearer, "cant disable apc!")
+		return
+
+	var/list/things_to_disrupt = list(target)
+	if(isliving(target))
+		var/mob/living/live_target = target
+		things_to_disrupt += live_target.get_all_gear()
+
+	for(var/atom/disrupted as anything in things_to_disrupt)
+		if(disrupted.on_saboteur(src, 1 MINUTES))
+			mod.add_charge(DEFAULT_CHARGE_DRAIN * 250)
+
+/obj/item/mod/module/stealth/wraith/on_suit_activation()
+	if(bumpoff)
+		RegisterSignal(mod.wearer, COMSIG_LIVING_MOB_BUMP, PROC_REF(unstealth))
+	RegisterSignal(mod.wearer, COMSIG_LIVING_UNARMED_ATTACK, PROC_REF(on_unarmed_attack))
+	RegisterSignal(mod.wearer, COMSIG_ATOM_BULLET_ACT, PROC_REF(on_bullet_act))
+	RegisterSignals(mod.wearer, list(COMSIG_MOB_ITEM_ATTACK, COMSIG_ATOM_ATTACKBY, COMSIG_ATOM_ATTACK_HAND, COMSIG_ATOM_HITBY, COMSIG_ATOM_HULK_ATTACK, COMSIG_ATOM_ATTACK_PAW, COMSIG_CARBON_CUFF_ATTEMPTED), PROC_REF(unstealth))
+	animate(mod.wearer, alpha = stealth_alpha, time = 1.5 SECONDS)
+	drain_power(use_energy_cost)
+
+/obj/item/mod/module/stealth/wraith/on_suit_deactivation(deleting)
+	if(bumpoff)
+		UnregisterSignal(mod.wearer, COMSIG_LIVING_MOB_BUMP)
+	UnregisterSignal(mod.wearer, list(COMSIG_LIVING_UNARMED_ATTACK, COMSIG_MOB_ITEM_ATTACK, COMSIG_ATOM_ATTACKBY, COMSIG_ATOM_ATTACK_HAND, COMSIG_ATOM_BULLET_ACT, COMSIG_ATOM_HITBY, COMSIG_ATOM_HULK_ATTACK, COMSIG_ATOM_ATTACK_PAW, COMSIG_CARBON_CUFF_ATTEMPTED))
+	animate(mod.wearer, alpha = 255, time = 1.5 SECONDS)
+
+/obj/item/mod/module/stealth/wraith/unstealth(datum/source)
+	. = ..()
+	if(mod.active)
+		addtimer(CALLBACK(src, PROC_REF(on_suit_activation)), 5 SECONDS)
+
+/obj/item/mod/module/stealth/wraith/examine_more(mob/user)
+	. = ..()
+	. += span_info( \
+		"The Wraith Module does not simply bend light around the user to obscure their visual pattern, \
+		but actively attacks and overloads surrounding light emitting objects, repurposing this energy to power the suit. \
+		It is possible that this technology has its origins in Spider Clan advancements, \
+		but the exact source of the Wraith Module is highly disputed. \
+		No group has stepped forward to claim it as their handiwork due to the political consequences of having stolen Spider Clan tech and their inevitable retaliation for such transgressions. \
+		Most point fingers at Cybersun Industries, but murmurs suggest it could even be even more clandestine organizations amongst the Syndicate branches. \
+		Whatever the case, if you are looking at one of these right now, don't show it to a space ninja." \
+	)

Monkestation/Monkestation2.0

code/modules/mod/modules/modules_antag.dm
@@ -504,3 +504,63 @@ monkestation end */
 	if(deleting)
 		return
 	mod.helmet.flash_protect = initial(mod.helmet.flash_protect)
+
+/obj/item/mod/module/stealth/wraith
+	name = "MOD Wraith Cloaking Module"
+	desc = "A more destructive adaptation of the stealth module."
+	icon_state = "cloak_traitor"
+	stealth_alpha = 30
+	module_type = MODULE_ACTIVE
+	cooldown_time = 2 SECONDS
+
+/obj/item/mod/module/stealth/wraith/on_select_use(atom/target)
+	. = ..()
+	if(!. || target == mod.wearer)
+		return
+	if(get_dist(mod.wearer, target) > 6)
+		balloon_alert(mod.wearer, "can't reach that!")
+		return
+	if(istype(target, /obj/machinery/power/apc)) //Bit too strong for a module so this is blacklisted
+		balloon_alert(mod.wearer, "cant disable apc!")
+		return
+
+	var/list/things_to_disrupt = list(target)
+	if(iscarbon(target))
+		var/mob/living/carbon/carbon_target = target
+		things_to_disrupt += carbon_target.get_all_gear()
+
+	for(var/atom/disrupted as anything in things_to_disrupt)
+		if(disrupted.on_saboteur(src, 1 MINUTES))
+			mod.add_charge(DEFAULT_CHARGE_DRAIN * 250)
+
+/obj/item/mod/module/stealth/wraith/on_suit_activation()
+	if(bumpoff)
+		RegisterSignal(mod.wearer, COMSIG_LIVING_MOB_BUMP, PROC_REF(unstealth))
+	RegisterSignal(mod.wearer, COMSIG_LIVING_UNARMED_ATTACK, PROC_REF(on_unarmed_attack))
+	RegisterSignal(mod.wearer, COMSIG_ATOM_BULLET_ACT, PROC_REF(on_bullet_act))
+	RegisterSignals(mod.wearer, list(COMSIG_MOB_ITEM_ATTACK, COMSIG_ATOM_ATTACKBY, COMSIG_ATOM_ATTACK_HAND, COMSIG_ATOM_HITBY, COMSIG_ATOM_HULK_ATTACK, COMSIG_ATOM_ATTACK_PAW, COMSIG_CARBON_CUFF_ATTEMPTED), PROC_REF(unstealth))
+	animate(mod.wearer, alpha = stealth_alpha, time = 1.5 SECONDS)
+	drain_power(use_power_cost)
+
+/obj/item/mod/module/stealth/wraith/on_suit_deactivation(deleting)
+	if(bumpoff)
+		UnregisterSignal(mod.wearer, COMSIG_LIVING_MOB_BUMP)
+	UnregisterSignal(mod.wearer, list(COMSIG_LIVING_UNARMED_ATTACK, COMSIG_MOB_ITEM_ATTACK, COMSIG_ATOM_ATTACKBY, COMSIG_ATOM_ATTACK_HAND, COMSIG_ATOM_BULLET_ACT, COMSIG_ATOM_HITBY, COMSIG_ATOM_HULK_ATTACK, COMSIG_ATOM_ATTACK_PAW, COMSIG_CARBON_CUFF_ATTEMPTED))
+	animate(mod.wearer, alpha = 255, time = 1.5 SECONDS)
+
+/obj/item/mod/module/stealth/wraith/unstealth(datum/source)
+	. = ..()
+	if(mod.active)
+		addtimer(CALLBACK(src, PROC_REF(on_suit_activation)), 5 SECONDS)
+
+/obj/item/mod/module/stealth/wraith/examine_more(mob/user)
+	. = ..()
+	. += span_info( \
+		"The Wraith Module does not simply bend light around the user to obscure their visual pattern, \
+		but actively attacks and overloads surrounding light emitting objects, repurposing this energy to power the suit. \
+		It is possible that this technology has its origins in Spider Clan advancements, \
+		but the exact source of the Wraith Module is highly disputed. \
+		No group has stepped forward to claim it as their handiwork due to the political consequences of having stolen Spider Clan tech and their inevitable retaliation for such transgressions. \
+		Most point fingers at Cybersun Industries, but murmurs suggest it could even be even more clandestine organizations amongst the Syndicate branches. \
+		Whatever the case, if you are looking at one of these right now, don't show it to a space ninja." \
+	)