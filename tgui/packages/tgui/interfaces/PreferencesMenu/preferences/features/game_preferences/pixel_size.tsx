ddf10f3ac1013fc4f2284df0b5586d1f3f32b30e
Ice-Type <1c3Type@gmail.com>
2025-07-13 23:59:26 +0100

    NonIntegerFull4k

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/pixel_size.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/pixel_size.tsx
@@ -10,6 +10,7 @@ export const pixel_size: Feature<number> = {
     2: 'Pixel Perfect 2x',
     3: 'Pixel Perfect 3x',
     4: 'Pixel Perfect 4x',
+    4.5: 'Pixel Perfect 4.5x',
     5: 'Pixel Perfect 5x',
     6: 'Pixel Perfect 6x',
     7: 'Pixel Perfect 7x',
