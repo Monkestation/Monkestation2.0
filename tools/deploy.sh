5b230028d16196f90c55cf030d6f905d27440e15
Lucy <lucy@absolucy.moe>
2025-07-19 22:51:22 -0400

    oh fuck right off (this is why modularization sucks ass)

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -20,8 +20,7 @@ mkdir -p \
     $1/tgui/public \
     $1/tgui/packages/tgfont/dist \
     $1/monkestation/icons \
-    $1/monkestation/code/modules/blueshift \
-    $1/monkestation/code/modules/donator
+    $1/monkestation/code
 
 if [ -d ".git" ]; then
   mkdir -p $1/.git/logs
@@ -38,8 +37,7 @@ cp -r strings/* $1/strings/
 cp -r tgui/public/* $1/tgui/public/
 cp -r tgui/packages/tgfont/dist/* $1/tgui/packages/tgfont/dist/
 cp -r monkestation/icons/* $1/monkestation/icons/
-cp -r monkestation/code/modules/blueshift/* $1/monkestation/code/modules/blueshift/
-cp -r monkestation/code/modules/donator/* $1/monkestation/code/modules/donator/
+cp -r monkestation/code/* $1/monkestation/code/
 
 #remove .dm files from _maps
 
