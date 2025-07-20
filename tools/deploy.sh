9d69f20c7d1cb93fc5f41cbd4e09d71a854fe1ad
Lucy <lucy@absolucy.moe>
2025-07-19 23:08:35 -0400

    me when

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -19,6 +19,7 @@ mkdir -p \
     $1/strings \
     $1/tgui/public \
     $1/tgui/packages/tgfont/dist \
+    $1/goon/icons \
     $1/monkestation/icons \
     $1/monkestation/code
 
@@ -36,6 +37,7 @@ cp -r sound/runtime/* $1/sound/runtime/
 cp -r strings/* $1/strings/
 cp -r tgui/public/* $1/tgui/public/
 cp -r tgui/packages/tgfont/dist/* $1/tgui/packages/tgfont/dist/
+cp -r goon/icons/* $1/goon/icons/
 cp -r monkestation/icons/* $1/monkestation/icons/
 cp -r monkestation/code/* $1/monkestation/code/
 
