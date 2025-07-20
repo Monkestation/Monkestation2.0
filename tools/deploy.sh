a953ff240d41f7c2a4d1c455e948cae8f709a6fb
Lucy <lucy@absolucy.moe>
2025-07-19 22:36:04 -0400

    whoops!

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -19,6 +19,7 @@ mkdir -p \
     $1/strings \
     $1/tgui/public \
     $1/tgui/packages/tgfont/dist \
+		$1/monkestation/icons \
     $1/monkestation/code/modules/blueshift/gags \
     $1/monkestation/code/modules/donator
 
@@ -36,6 +37,7 @@ cp -r sound/runtime/* $1/sound/runtime/
 cp -r strings/* $1/strings/
 cp -r tgui/public/* $1/tgui/public/
 cp -r tgui/packages/tgfont/dist/* $1/tgui/packages/tgfont/dist/
+cp -r monkestation/icons/* $1/monkestation/icons/
 cp -r monkestation/code/modules/blueshift/gags/* $1/monkestation/code/modules/blueshift/gags/
 cp -r monkestation/code/modules/donator/* $1/monkestation/code/modules/donator/
 
