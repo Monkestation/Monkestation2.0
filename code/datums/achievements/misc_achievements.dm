HolyTigerGod/Voidcrew

Adds support for different achievement dmis (#81471)
## About The Pull Request

I am back to pushing my code improvements upstream, sorry in advance.
As the title says, this adds support for achievement icons to be in
different dmi files than the default, pretty much the exact same way
that Language icons (for chat assets) do.

## Why It's Good For The Game

It is one of the few things in game (the only other thing i can think of
that does this rn is barsigns) that cannot have their icon changed when
trying to make a subtype, this is very limiting and very annoying,
especially since dmi conflicts are one of the worst types to deal with,
this would make my life a lot easier.

## Changelog

No player-facing changes.

code/datums/achievements/misc_achievements.dm
@@ -1,54 +1,54 @@
 /datum/award/achievement/misc
 	category = "Misc"
-	icon = "basemisc" //for those achievements that still need an actual icon, later.
+	icon_state = "basemisc" //for those achievements that still need an actual icon, later.
 
 /datum/award/achievement/misc/meteor_examine
 	name = "Your Life Before Your Eyes"
 	desc = "Take a close look at hurtling space debris"
 	database_id = MEDAL_METEOR
-	icon = "meteors"
+	icon_state = "meteors"
 
 /datum/award/achievement/misc/pulse
 	name = "Jackpot"
 	desc = "Win a pulse rifle from an arcade machine"
 	database_id = MEDAL_PULSE
-	icon = "jackpot"
+	icon_state = "jackpot"
 
 /datum/award/achievement/misc/time_waste
 	name = "Time waster"
 	desc = "Speak no evil, hear no evil, see just errors"
 	database_id = MEDAL_TIMEWASTE
-	icon = "timewaste"
+	icon_state = "timewaste"
 
 /datum/award/achievement/misc/round_and_full
 	name = "Round and Full"
 	desc = "Well at least you aren't down the river, I hear they eat people there."
 	database_id = MEDAL_CLOWNCARKING
-	icon = "clownking"
+	icon_state = "clownking"
 
 /datum/award/achievement/misc/the_best_driver
 	name = "The Best Driver"
 	desc = "100 honks later"
 	database_id = MEDAL_THANKSALOT
-	icon = "clownthanks"
+	icon_state = "clownthanks"
 
 /datum/award/achievement/misc/getting_an_upgrade
 	name = "Getting an upgrade"
 	desc = "Make your first unique material item!"
 	database_id = MEDAL_MATERIALCRAFT
-	icon = "upgrade"
+	icon_state = "upgrade"
 
 /datum/award/achievement/misc/rocket_holdup
 	name = "Disk, Please!"
 	desc = "Is the man currently pointing a loaded rocket launcher at your head point blank really dumb enough to pull the trigger? Do you really want to find out?"
 	database_id = MEDAL_DISKPLEASE
-	icon = "rocket_holdup"
+	icon_state = "rocket_holdup"
 
 /datum/award/achievement/misc/gamer
 	name = "My Watchlist Status is Not Important"
 	desc = "You may be under the impression that violent video games are a harmless pastime, but the security and medical personnel swarming your location with batons and knockout gas look like they disagree."
 	database_id = MEDAL_GAMER
-	icon = "live_sec_reaction"
+	icon_state = "live_sec_reaction"
 
 /datum/award/achievement/misc/vendor_squish
 	name = "I Was a Teenage Anarchist"
@@ -69,161 +69,161 @@
 	name = "One Lean, Mean, Cleaning Machine"
 	desc = "How does it feel to know that your workplace values a mop bucket on wheels more than you?" // i can do better than this give me time
 	database_id = MEDAL_CLEANBOSS
-	icon = "cleanboss"
+	icon_state = "cleanboss"
 
 /datum/award/achievement/misc/rule8
 	name = "Rule 8"
 	desc = "Call an admin this is ILLEGAL!!"
 	database_id = MEDAL_RULE8
-	icon = "rule8"
+	icon_state = "rule8"
 
 /datum/award/achievement/misc/speed_round
 	name = "Long shift"
 	desc = "Well, that didn't take long."
 	database_id = MEDAL_LONGSHIFT
-	icon = "longshift"
+	icon_state = "longshift"
 
 /datum/award/achievement/misc/lookoutsir
 	name = "Look Out, Sir!"
 	desc = "Either awarded for making the ultimate sacrifice for your comrades, or a really dumb attempt at grenade jumping."
 	database_id = MEDAL_LOOKOUTSIR
-	icon = "martyr" // purple heart on an explosive danger warning sign (well, sort of)
+	icon_state = "martyr" // purple heart on an explosive danger warning sign (well, sort of)
 
 /datum/award/achievement/misc/gottem
 	name = "HA, GOTTEM"
 	desc = "Made you look!"
 	database_id = MEDAL_GOTTEM
-	icon = "gottem"
+	icon_state = "gottem"
 
 /datum/award/achievement/misc/ascension
 	name = "Ascension"
 	desc = "Caedite eos. Novit enim Dominus qui sunt eius."
 	database_id = MEDAL_ASCENSION
-	icon = "ascension"
+	icon_state = "ascension"
 
 /datum/award/achievement/misc/ash_ascension
 	name = "Nightwatcher's Eyes"
 	desc = "You've risen above the flames, became one with the ashes. You've been reborn as one with the Nightwatcher."
 	database_id = MEDAL_ASH_ASCENSION
-	icon = "ashascend"
+	icon_state = "ashascend"
 
 /datum/award/achievement/misc/flesh_ascension
 	name = "Vortex of Arms"
 	desc = "You've became something more, something greater. A piece of the emperor resides within you, and you within him."
 	database_id = MEDAL_FLESH_ASCENSION
-	icon = "fleshascend"
+	icon_state = "fleshascend"
 
 /datum/award/achievement/misc/rust_ascension
 	name = "Hills of Rust"
 	desc = "You've summoned a piece of the Hill of rust, and so the Hills welcome you."
 	database_id = MEDAL_RUST_ASCENSION
-	icon = "rustascend"
+	icon_state = "rustascend"
 
 /datum/award/achievement/misc/void_ascension
 	name = "All that perish"
 	desc = "Place of a different being, different time. Everything ends there... but maybe it is just the beginning?"
 	database_id = MEDAL_VOID_ASCENSION
-	icon = "voidascend"
+	icon_state = "voidascend"
 
 /datum/award/achievement/misc/blade_ascension
 	name = "Silver and Steel"
 	desc = "You've become the master of all duellists - the paragon of blades."
 	database_id = MEDAL_BLADE_ASCENSION
-	icon = "bladeascend"
+	icon_state = "bladeascend"
 
 /datum/award/achievement/misc/cosmic_ascension
 	name = "It arrived"
 	desc = "You managed to teleport an entity on the station that really shouldn't be there."
 	database_id = MEDAL_COSMOS_ASCENSION
-	icon = "cosmicascend"
+	icon_state = "cosmicascend"
 
 /datum/award/achievement/misc/lock_ascension
 	name = "Secrets of the Locked Labyrinth"
 	desc = "You managed to open a gate into the mansus."
 	database_id = MEDAL_LOCK_ASCENSION
-	icon = "lockascend"
+	icon_state = "lockascend"
 
 /datum/award/achievement/misc/moon_ascension
 	name = "The Last Act"
 	desc = "You managed to become the ringleader and slay the lie."
 	database_id = MEDAL_MOON_ASCENSION
-	icon = "moonascend"
+	icon_state = "moonascend"
 
 /datum/award/achievement/misc/grand_ritual_finale
 	name = "Archmage"
 	desc = "Made a big impression on the station with your phenomenal cosmic power."
 	database_id = MEDAL_ARCHMAGE
-	icon = "archmage"
+	icon_state = "archmage"
 
 /datum/award/achievement/misc/toolbox_soul
 	name = "SOUL'd Out"
 	desc = "My eternal soul was destroyed to make a toolbox look funny and all I got was this achievement..."
 	database_id = MEDAL_TOOLBOX_SOUL
-	icon = "toolbox_soul"
+	icon_state = "toolbox_soul"
 
 /datum/award/achievement/misc/hot_damn
 	name = "Hot Damn!"
 	desc = "Sometimes you need to make some noise to make a point."
 	database_id = MEDAL_HOT_DAMN
-	icon = "hotdamn"
+	icon_state = "hotdamn"
 
 /datum/award/achievement/misc/cayenne_disk
 	name = "Very Important Piscis"
 	desc = "You can rest well now."
 	database_id = MEDAL_CAYENNE_DISK
-	icon = "cayenne_disk"
+	icon_state = "cayenne_disk"
 
 /datum/award/achievement/misc/tram_surfer
 	name = "Tram Surfer"
 	desc = "Lights out, guerilla radio!"
 	database_id = MEDAL_TRAM_SURFER
-	icon = "tram_surfer"
+	icon_state = "tram_surfer"
 
 /datum/award/achievement/misc/cult_shuttle_omfg
 	name = "WHAT JUST HAPPENED"
 	desc = "As a blood cultist, be part of a team that summons 3 shuttle curses within 10 seconds. Imagine cleaning up after them, g r o s s!"
 	database_id = MEDAL_CULT_SHUTTLE_OMFG
-	icon = "cult_shuttle_omfg"
+	icon_state = "cult_shuttle_omfg"
 
 /datum/award/achievement/misc/clickbait
 	name = "Clickbait"
 	desc = "Where's my free smartphone?!?"
 	database_id = MEDAL_CLICKBAIT
-	icon = "bait"
+	icon_state = "bait"
 
 /datum/award/achievement/misc/narsupreme
 	name = "If Nar'Sie is so good, why isn't there a..."
 	desc = "Even interdimensional space deitys need a friend."
 	database_id = MEDAL_NARSUPREME
-	icon = "narsupreme"
+	icon_state = "narsupreme"
 
 /datum/award/achievement/misc/springlock
 	name = "The Man Inside the MODsuit"
 	desc = "Ignore the warning label on a springlock MODsuit."
 	database_id = MEDAL_SPRINGLOCK
-	icon = "springlock"
+	icon_state = "springlock"
 
 /datum/award/achievement/misc/healthy
 	name = "The Picture of Health"
 	desc = "Don't be such a baby, it's just a heart attack. You've bounced back from worse!"
 	database_id = MEDAL_HEALTHY
-	icon = "picofhealth"
+	icon_state = "picofhealth"
 
 /datum/award/achievement/misc/gods_wrath
 	name = "God's Wrath"
 	desc = "Did you think you could get away with defiling the word of God?"
 	database_id = MEDAL_GODS_WRATH
-	icon = "godswrath"
+	icon_state = "godswrath"
 
 /datum/award/achievement/misc/earthquake_victim
 	name = "A Nasty Fall"
 	desc = "...And the earth opened its mouth and swallowed them and their station- all the HOP's men and all their possessions."
 	database_id = MEDAL_EARTHQUAKE_VICTIM
-	icon = "earthquake"
+	icon_state = "earthquake"
 
 /datum/award/achievement/misc/debt_extinguished
 	name = "Outdebted"
 	desc = "I've paid my dues, shift after shift... I've done my sentence but commited no griff..."
 	database_id = MEDAL_DEBT_EXTINGUISHED
-	icon = "outdebted"
+	icon_state = "outdebted"
 

Monkestation/Monkestation2.0

code/datums/achievements/misc_achievements.dm
@@ -1,48 +1,48 @@
 /datum/award/achievement/misc
 	category = "Misc"
-	icon = "basemisc" //for those achievements that still need an actual icon, later.
+	icon_state = "basemisc" //for those achievements that still need an actual icon, later.
 
 /datum/award/achievement/misc/meteor_examine
 	name = "Your Life Before Your Eyes"
 	desc = "Take a close look at hurtling space debris"
 	database_id = MEDAL_METEOR
-	icon = "meteors"
+	icon_state = "meteors"
 
 /datum/award/achievement/misc/pulse
 	name = "Jackpot"
 	desc = "Win a pulse rifle from an arcade machine"
 	database_id = MEDAL_PULSE
-	icon = "jackpot"
+	icon_state = "jackpot"
 
 /datum/award/achievement/misc/time_waste
 	name = "Time waster"
 	desc = "Speak no evil, hear no evil, see just errors"
 	database_id = MEDAL_TIMEWASTE
-	icon = "timewaste"
+	icon_state = "timewaste"
 
 /datum/award/achievement/misc/round_and_full
 	name = "Round and Full"
 	desc = "Well at least you aren't down the river, I hear they eat people there."
 	database_id = MEDAL_CLOWNCARKING
-	icon = "clownking"
+	icon_state = "clownking"
 
 /datum/award/achievement/misc/the_best_driver
 	name = "The Best Driver"
 	desc = "100 honks later"
 	database_id = MEDAL_THANKSALOT
-	icon = "clownthanks"
+	icon_state = "clownthanks"
 
 /datum/award/achievement/misc/getting_an_upgrade
 	name = "Getting an upgrade"
 	desc = "Make your first unique material item!"
 	database_id = MEDAL_MATERIALCRAFT
-	icon = "upgrade"
+	icon_state = "upgrade"
 
 /datum/award/achievement/misc/rocket_holdup
 	name = "Disk, Please!"
 	desc = "Is the man currently pointing a loaded rocket launcher at your head point blank really dumb enough to pull the trigger? Do you really want to find out?"
 	database_id = MEDAL_DISKPLEASE
-	icon = "rocket_holdup"
+	icon_state = "rocket_holdup"
 
 /datum/award/achievement/misc/gamer
 	name = "My Watchlist Status is Not Important"
@@ -73,148 +73,148 @@
 	name = "Rule 3"
 	desc = "Call an admin this is ILLEGAL!!"
 	database_id = MEDAL_RULE8 //blame tg lmao
-	icon = "rule8"
+	icon_state = "rule8"
 
 /datum/award/achievement/misc/speed_round
 	name = "Long shift"
 	desc = "Well, that didn't take long."
 	database_id = MEDAL_LONGSHIFT
-	icon = "longshift"
+	icon_state = "longshift"
 
 /datum/award/achievement/misc/lookoutsir
 	name = "Look Out, Sir!"
 	desc = "Either awarded for making the ultimate sacrifice for your comrades, or a really dumb attempt at grenade jumping."
 	database_id = MEDAL_LOOKOUTSIR
-	icon = "martyr" // purple heart on an explosive danger warning sign (well, sort of)
+	icon_state = "martyr" // purple heart on an explosive danger warning sign (well, sort of)
 
 /datum/award/achievement/misc/gottem
 	name = "HA, GOTTEM"
 	desc = "Made you look!"
 	database_id = MEDAL_GOTTEM
-	icon = "gottem"
+	icon_state = "gottem"
 
 /datum/award/achievement/misc/ascension
 	name = "Ascension"
 	desc = "Caedite eos. Novit enim Dominus qui sunt eius."
 	database_id = MEDAL_ASCENSION
-	icon = "ascension"
+	icon_state = "ascension"
 
 /datum/award/achievement/misc/ash_ascension
 	name = "Nightwatcher's Eyes"
 	desc = "You've risen above the flames, became one with the ashes. You've been reborn as one with the Nightwatcher."
 	database_id = MEDAL_ASH_ASCENSION
-	icon = "ashascend"
+	icon_state = "ashascend"
 
 /datum/award/achievement/misc/flesh_ascension
 	name = "Vortex of Arms"
 	desc = "You've became something more, something greater. A piece of the emperor resides within you, and you within him."
 	database_id = MEDAL_FLESH_ASCENSION
-	icon = "fleshascend"
+	icon_state = "fleshascend"
 
 /datum/award/achievement/misc/rust_ascension
 	name = "Hills of Rust"
 	desc = "You've summoned a piece of the Hill of rust, and so the Hills welcome you."
 	database_id = MEDAL_RUST_ASCENSION
-	icon = "rustascend"
+	icon_state = "rustascend"
 
 /datum/award/achievement/misc/void_ascension
 	name = "All that perish"
 	desc = "Place of a different being, different time. Everything ends there... but maybe it is just the beginning?"
 	database_id = MEDAL_VOID_ASCENSION
-	icon = "voidascend"
+	icon_state = "voidascend"
 
 /datum/award/achievement/misc/blade_ascension
 	name = "Silver and Steel"
 	desc = "You've become the master of all duellists - the paragon of blades."
 	database_id = MEDAL_BLADE_ASCENSION
-	icon = "bladeascend"
+	icon_state = "bladeascend"
 
 /datum/award/achievement/misc/cosmic_ascension
 	name = "It arrived"
 	desc = "You managed to teleport an entity on the station that really shouldn't be there."
 	database_id = MEDAL_COSMOS_ASCENSION
-	icon = "cosmicascend"
+	icon_state = "cosmicascend"
 
 /datum/award/achievement/misc/moon_ascension
 	name = "The Last Act"
 	desc = "You managed to become the ringleader and slay the lie."
 	database_id = MEDAL_MOON_ASCENSION
-	icon = "moonascend"
+	icon_state = "moonascend"
 
 /datum/award/achievement/misc/grand_ritual_finale
 	name = "Archmage"
 	desc = "Made a big impression on the station with your phenomenal cosmic power."
 	database_id = MEDAL_ARCHMAGE
-	icon = "archmage"
+	icon_state = "archmage"
 
 /datum/award/achievement/misc/knock_ascension
 	name = "Secrets behind the Spider Door"
 	desc = "You managed to open a gate into the mansus."
 	database_id = MEDAL_KNOCK_ASCENSION
-	icon = "knockascend"
+	icon_state = "knockascend"
 
 /datum/award/achievement/misc/toolbox_soul
 	name = "SOUL'd Out"
 	desc = "My eternal soul was destroyed to make a toolbox look funny and all I got was this achievement..."
 	database_id = MEDAL_TOOLBOX_SOUL
-	icon = "toolbox_soul"
+	icon_state = "toolbox_soul"
 
 /datum/award/achievement/misc/hot_damn
 	name = "Hot Damn!"
 	desc = "Sometimes you need to make some noise to make a point."
 	database_id = MEDAL_HOT_DAMN
-	icon = "hotdamn"
+	icon_state = "hotdamn"
 
 /datum/award/achievement/misc/cayenne_disk
 	name = "Very Important Piscis"
 	desc = "You can rest well now."
 	database_id = MEDAL_CAYENNE_DISK
-	icon = "cayenne_disk"
+	icon_state = "cayenne_disk"
 
 /datum/award/achievement/misc/tram_surfer
 	name = "Tram Surfer"
 	desc = "Lights out, guerilla radio!"
 	database_id = MEDAL_TRAM_SURFER
-	icon = "tram_surfer"
+	icon_state = "tram_surfer"
 
 /datum/award/achievement/misc/cult_shuttle_omfg
 	name = "WHAT JUST HAPPENED"
 	desc = "As a blood cultist, be part of a team that summons 3 shuttle curses within 10 seconds. Imagine cleaning up after them, g r o s s!"
 	database_id = MEDAL_CULT_SHUTTLE_OMFG
-	icon = "cult_shuttle_omfg"
+	icon_state = "cult_shuttle_omfg"
 
 /datum/award/achievement/misc/clickbait
 	name = "Clickbait"
 	desc = "Where's my free smartphone?!?"
 	database_id = MEDAL_CLICKBAIT
-	icon = "bait"
+	icon_state = "bait"
 
 /datum/award/achievement/misc/narsupreme
 	name = "If Nar'Sie is so good, why isn't there a..."
 	desc = "Even interdimensional space deitys need a friend."
 	database_id = MEDAL_NARSUPREME
-	icon = "narsupreme"
+	icon_state = "narsupreme"
 
 /datum/award/achievement/misc/springlock
 	name = "The Man Inside the MODsuit"
 	desc = "Ignore the warning label on a springlock MODsuit."
 	database_id = MEDAL_SPRINGLOCK
-	icon = "springlock"
+	icon_state = "springlock"
 
 /datum/award/achievement/misc/healthy
 	name = "The Picture of Health"
 	desc = "Don't be such a baby, it's just a heart attack. You've bounced back from worse!"
 	database_id = MEDAL_HEALTHY
-	icon = "picofhealth"
+	icon_state = "picofhealth"
 
 /datum/award/achievement/misc/debt_extinguished
 	name = "Outdebted"
 	desc = "I've paid my dues, shift after shift... I've done my sentence but commited no griff..."
 	database_id = MEDAL_DEBT_EXTINGUISHED
-	icon = "outdebted"
+	icon_state = "outdebted"
 
 /datum/award/achievement/misc/gods_wrath
 	name = "God's Wrath"
 	desc = "Did you think you could get away with defiling the word of God?"
 	database_id = MEDAL_GODS_WRATH
-	icon = "godswrath"
+	icon_state = "godswrath"