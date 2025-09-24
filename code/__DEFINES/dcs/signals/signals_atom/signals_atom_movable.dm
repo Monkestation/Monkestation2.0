voidcrew/Voidcrew

Fixes chasm jaunters not working while you're buckled to a mob or an object (#92693)
## About The Pull Request

Now all mobs are unbuckled from falling objects and dropped
individually, similarly to how lava functions. Also updated jaunters to
be comsig-based rather than chasms snowflake checking for jaunters in
belt slots.
Closes #92663

code/__DEFINES/dcs/signals/signals_atom/signals_atom_movable.dm
@@ -138,3 +138,8 @@
 #define COMSIG_MOVABLE_BUMP_PUSHED "movable_bump_pushed"
 	/// Stop it from moving
 	#define COMPONENT_NO_PUSH (1<<0)
+
+/// Called when the atom is dropped into a chasm: (turf/chasm)
+#define COMSIG_MOVABLE_CHASM_DROPPED "movable_charm_dropped"
+	/// Stop it from actually dropping into the chasm
+	#define COMPONENT_NO_CHASM_DROP (1<<0)

Monkestation/Monkestation2.0

code/__DEFINES/dcs/signals/signals_atom/signals_atom_movable.dm
@@ -110,3 +110,8 @@
 	#define MOVABLE_SAY_QUOTE_MESSAGE_MODS 3
 
 #define COMSIG_MOB_CI_TOGGLED "mob_ci_toggled"
+
+/// Called when the atom is dropped into a chasm: (turf/chasm)
+#define COMSIG_MOVABLE_CHASM_DROPPED "movable_chasm_dropped"
+	/// Stop it from actually dropping into the chasm
+	#define COMPONENT_NO_CHASM_DROP (1<<0)