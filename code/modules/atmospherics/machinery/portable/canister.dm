HolyTigerGod/Voidcrew

Use cell defined constants for various stuff (#82594)
## About The Pull Request
This re writes most cell power usage cases with 2 defines
`STANDARD_CELL_CHARGE`(Joules) & `STANDARD_CELL_RATE`(Watts) so changing
cell capacity values in the future won't cause discrepancies.

## Changelog
:cl:
code: most cell power usages are scaled with defined constants to help
adapt to future changes
/:cl:

code/modules/atmospherics/machinery/portable/canister.dm
@@ -483,7 +483,7 @@
 		var/energy_consumed = energy_factor * 250 * seconds_per_tick
 		if(powered(AREA_USAGE_EQUIP, ignore_use_power = TRUE))
 			use_energy(energy_consumed, channel = AREA_USAGE_EQUIP)
-		else if(!internal_cell?.use(energy_consumed * 0.025 KILO JOULES))
+		else if(!internal_cell?.use(energy_consumed * 0.025))
 			shielding_powered = FALSE
 			SSair.start_processing_machine(src)
 			investigate_log("shielding turned off due to power loss")

Monkestation/Monkestation2.0

code/modules/atmospherics/machinery/portable/canister.dm
@@ -526,9 +526,7 @@ GLOBAL_LIST_INIT(gas_id_to_canister, init_gas_id_to_canister())
 		if(powered(AREA_USAGE_EQUIP, ignore_use_power = TRUE))
 			use_energy(power_consumed, AREA_USAGE_EQUIP)
 			protected_contents = TRUE
-		else if(internal_cell?.use(power_consumed * 0.025))
-			protected_contents = TRUE
-		else
+		else if(!internal_cell?.use(power_consumed * 0.025))
 			shielding_powered = FALSE
 			SSair.start_processing_machine(src)
 			investigate_log("shielding turned off due to power loss", INVESTIGATE_ATMOS)