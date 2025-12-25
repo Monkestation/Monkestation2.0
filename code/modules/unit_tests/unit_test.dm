HolyTigerGod/Voidcrew

Camera eyes have been lightly refactored  (among other things...) (#87805)
## About The Pull Request

* A generic /mob/eye/camera type has been made, containing everything
needed to interface with a cameranet
* /mob/eye/ai_eye has been refactored into a generic /mob/eye/camera
instance
* Advanced cameras no longer inherit from AI eyes, splitting off
behaviour
* Camera code has been somewhat cleaned up
* Probably some more stuff I'm forgetting right now

## Big man Southport:

![image](https://github.com/user-attachments/assets/de6e6ff0-ca99-4636-bdec-5e0b2d6b8037)

## Changelog

:cl:
code: made /proc/getviewsize() pure

refactor: mob/eye/ai_eye has been restructured, now inheriting from a
generic mob/eye/camera type
refactor: advanced cameras and their subtypes are now
mob/eye/camera/remote subtypes
code: the cameranet no longer expects the user to be an AI eye
code: remote camera eyes have had their initialization streamlined
code: remote cameras handle assigning and unassigning users by
themselves now
code: remote cameras now use weakrefs instead of hard referencing owners
and origins
code: also the sentient disease is_define was removed (we don't have
those anymore)
fix: AI eyes no longer assign real names to themselves, fixing their
orbit name
/:cl:

---------

Co-authored-by: Ghom <42542238+Ghommie@users.noreply.github.com>

code/modules/unit_tests/unit_test.dm
@@ -289,9 +289,9 @@ GLOBAL_VAR_INIT(focused_tests, focused_tests())
 	//No heart to give
 	returnable_list += typesof(/obj/structure/ethereal_crystal)
 	//No linked console
-	returnable_list += typesof(/mob/eye/ai_eye/remote/base_construction)
+	returnable_list += typesof(/mob/eye/camera/remote/base_construction)
 	//See above
-	returnable_list += typesof(/mob/eye/ai_eye/remote/shuttle_docker)
+	returnable_list += typesof(/mob/eye/camera/remote/shuttle_docker)
 	//Hangs a ref post invoke async, which we don't support. Could put a qdeleted check but it feels hacky
 	returnable_list += typesof(/obj/effect/anomaly/grav/high)
 	//See above

Monkestation/Monkestation2.0

code/modules/unit_tests/unit_test.dm
@@ -297,9 +297,9 @@ GLOBAL_VAR_INIT(focused_tests, focused_tests())
 	//No heart to give
 	ignore += typesof(/obj/structure/ethereal_crystal)
 	//No linked console
-	ignore += typesof(/mob/eye/ai_eye/remote/base_construction)
+	ignore += typesof(/mob/eye/camera/remote/base_construction)
 	//See above
-	ignore += typesof(/mob/eye/ai_eye/remote/shuttle_docker)
+	ignore += typesof(/mob/eye/camera/remote/shuttle_docker)
 	//Hangs a ref post invoke async, which we don't support. Could put a qdeleted check but it feels hacky
 	ignore += typesof(/obj/effect/anomaly/grav/high)
 	//See above