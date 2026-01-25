e9f46952b8efc8453e426c428de2bccc54abeda9
Lucy <lucy@absolucy.moe>
2026-01-24 21:19:49 -0500

    meow?

--- tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/vampiricaccident.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/vampiricaccident.ts
@@ -2,16 +2,16 @@ import { Antagonist, Category } from '../base';
 import { multiline } from 'common/string';
 
 const VampiricAccident: Antagonist = {
-  key: 'vampiriaccident',
+  key: 'vampiricaccident',
   name: 'Vampiric Accident',
   description: [
     multiline`
       After your death, you awaken to see yourself as an undead monster.
-      Scrape by Space Station 13, or take it over, ruling from the shadows!
+      Use your Vampiric abilities as best you can.
+      Scrape by Space Station 13, or take over it, vassalizing your way.
     `,
   ],
   category: Category.Midround,
-  priority: -1,
 };
 
 export default VampiricAccident;
